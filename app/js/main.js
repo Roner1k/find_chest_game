364fda5662f043f86272c8afa17a703fdcd1ccff
Mikolay <xzxronerxzx@gmail.com>
2020-03-23 00:02:58 +0200

    hw9 complete 23.03.20

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 $(document).ready(function () {
 
     $('.events-wrap__item').hover(function () {
@@ -8,5 +10,31 @@ $(document).ready(function () {
             $('.event-detail-button.active').fadeOut();
             $('.event-detail-button', this).removeClass('active');
         });
+
+
+    // let condition = 0;
+    // while (condition < 10) {
+    //     console.log(`<h3> делаем что-то + $(condition) раз`);
+    //     document.write(`<h3> делаем что-то + $(condition) раз`);
+    //     condition++;
+
+    //     let condition = 0;
+    //     do {
+    //         console.log(`<h3> делаем что-то + $(condition) раз`);
+    //         document.write(`<h3> делаем что-то + $(condition) раз`);
+    //         condition++;
+    //     }
+    // }
+    // while (condition < 10) ;
+
+
+    // let valueN = prompt("Введите СТОП-число. От 2 и больше (Четное)", 0);
+    // valueN = parseInt(valueN);
+    // for (let i = 2; i <= valueN; i++) {
+    //     if (i % 2 == 0) {
+    //         document.write(`<h3> Чет  ${i} раз </h3>`);
+    //     }
+    // }
+
 
 });
