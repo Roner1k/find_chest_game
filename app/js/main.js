d9fc1ab97f581b8de0d95da61c34d42158e02190
Mikolay <xzxronerxzx@gmail.com>
2020-03-22 12:28:09 +0200

    praktika 1

--- app/js/main.js
+++ app/js/main.js
@@ -2,15 +2,11 @@ $(document).ready(function () {
 
     $('.events-wrap__item').hover(function () {
             $('.event-detail-button', this).addClass('active');
-
             $('.event-detail-button.active').fadeIn();
         },
-
         function () {
             $('.event-detail-button.active').fadeOut();
             $('.event-detail-button', this).removeClass('active');
-
-
         });
 
 });
