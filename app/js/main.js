a0b0133f3dcd297eb97872d8224aeddec6b559c1
Nikolay Lesko <roner1kk@gmil.com>
2020-04-14 12:39:05 +0300

    HW15

--- app/js/main.js
+++ app/js/main.js
@@ -1,189 +1,222 @@
 'use strict'
+let myCar = {};
 
-//1 if
-function First() {
+myCar = {
+    manufacturer: 'Toyota',
+    model: 'AE86',
+    year: 1987,
+    weight: 923 + ' kg',
+    engine: '1.6 L 4A-GEU I4 130 HP ',
+    topSpeed: 200 + " km/h",
+    averageSpeed: 100 + " km/h",
+};
 
-    let NumberTest = +document.getElementById('test-number').value;
-    if (NumberTest > 0) {
-        document.getElementById('result-one').value = 'Положительное число!';
+function autoInfo() {
+
+    let myCarInfo = document.createElement('div');
+    myCarInfo.className = 'car-information';
+    myCarInfo.style.display = "grid";
+    myCarInfo.style.gridTemplateColumns = "1fr 2fr";
+    myCarInfo.style.alignSelf = 'center';
+    myCarInfo.style.justifyItems = 'center';
+    myCarInfo.style.border = "1px solid black";
+    myCarInfo.style.width = "666px";
+    myCarInfo.style.height = "340px";
+    myCarInfo.style.margin = "10px auto";
+
+    for (let key in myCar) {
+        let carProperty = document.createElement('div'),
+            carPropValue = document.createElement('div');
+        carProperty.style.border = '1px solid black';
+        carProperty.style.width = '100%';
+        carProperty.style.padding = '10px';
+        carPropValue.style.border = '1px solid black';
+        carPropValue.style.width = '100%';
+        carPropValue.style.padding = '10px';
+
+        carProperty.innerText = key[0].toUpperCase() + key.slice(1) + ":";
+        carPropValue.innerText = myCar[key];
+
+        myCarInfo.append(carProperty);
+        myCarInfo.append(carPropValue);
+
+        console.log(carProperty + typeof (carProperty));
     }
-    if (NumberTest < 0) {
-        document.getElementById('result-one').value = 'Отрицательное число!';
+    document.querySelector('figure').append(myCarInfo);
+};
+
+function AverageSpeedCalc() {
+    let averageSpeed = parseInt(myCar.averageSpeed),
+        distance = +document.querySelector('input').value,
+        nTime,
+        t;
+
+    nTime = distance / averageSpeed;
+    t = nTime;
+
+    for (let i = 4; i <= t; t -= 4) {
+        nTime++;
     }
-    if (NumberTest === 0) {
-        document.getElementById('result-one').value = 'А это ноль!';
-
-    }
-
-
-    console.log(typeof (userAge) + userAge);
+    alert('You need to ride: ' + Math.floor(nTime) + ' Hour(s)');
 };
 
 //2
-function Second() {
-    let userAge = +document.getElementById('user-age').value;
-    if (userAge > 0 && userAge < 120) {
-        document.getElementById('result-two').value = ' Корректный ввод! ';
 
-    } else document.getElementById('result-two').value = ' НЕ корректный ввод! ';
+function secondFunction() {
+    let firstNumbTop = +document.getElementById('f-n-top').value,
+        firstNumbBottom = +document.getElementById('f-n-bottom').value,
+        secNumbTop = +document.getElementById('s-n-top').value,
+        secNumbBottom = +document.getElementById('s-n-bottom').value,
+        actValue = document.getElementById('act-value').value;
 
+    let firstObj = {
+        topNumber: firstNumbTop,
+        bottomNumber: firstNumbBottom,
+    };
+    let secondObj = {
+        topNumber: secNumbTop,
+        bottomNumber: secNumbBottom,
+    };
+    let resultObj = {
+        resultTop: null,
+        resultBottom: null,
+    };
+
+    if (actValue !== "+" && actValue !== "-" && actValue !== "*" && actValue !== "/") {
+        alert("Проверьте ввод!")
+    } else {
+        switch (actValue) {
+            case "+":
+                sum();
+                break;
+            case "-":
+                subtraction();
+                break;
+            case "*":
+                multiplication();
+                break;
+            case "/":
+                segmentation();
+                break;
+            default:
+                alert("vse poshlo ne tak");
+        }
+
+        function sum() {
+            if (firstObj.bottomNumber === secondObj.bottomNumber) {
+                resultObj.resultTop = firstObj.topNumber + secondObj.topNumber;
+                resultObj.resultBottom = firstObj.bottomNumber;
+            } else {
+                resultObj.resultTop = (firstObj.topNumber * secondObj.bottomNumber) + (secondObj.topNumber * firstObj.bottomNumber);
+                resultObj.resultBottom = firstObj.bottomNumber * secondObj.bottomNumber;
+            }
+        }
+
+        function subtraction() {
+            if (firstObj.bottomNumber === secondObj.bottomNumber) {
+                resultObj.resultTop = firstObj.topNumber - secondObj.topNumber;
+                resultObj.resultBottom = firstObj.bottomNumber;
+            } else {
+                resultObj.resultTop = (firstObj.topNumber * secondObj.bottomNumber) - (secondObj.topNumber * firstObj.bottomNumber);
+                resultObj.resultBottom = firstObj.bottomNumber * secondObj.bottomNumber;
+            }
+        }
+
+        function multiplication() {
+            resultObj.resultTop = firstObj.topNumber * secondObj.topNumber;
+            resultObj.resultBottom = firstObj.bottomNumber * secondObj.bottomNumber;
+        }
+
+        function segmentation() {
+            resultObj.resultTop = firstObj.topNumber * secondObj.bottomNumber;
+            resultObj.resultBottom = firstObj.bottomNumber * secondObj.topNumber;
+        }
+
+        if (resultObj.resultTop < 0) {
+            resultObj.resultTop = resultObj.resultTop * -1;
+            document.getElementById('result-int').innerText = "-";
+        }
+
+        if (resultObj.resultTop % resultObj.resultBottom === 0) {
+            let resultInt = resultObj.resultTop / resultObj.resultBottom;
+            resultObj.resultTop = 0;
+            resultObj.resultBottom = 0;
+            document.getElementById('result-int').innerText += resultInt;
+        }
+
+        document.getElementById('res-top').value = resultObj.resultTop;
+        document.getElementById('res-bottom').value = resultObj.resultBottom;
+    }
+    ;
 };
+
+// function reduceFrac(numerator, denomerator) {
+//     let a = numerator, b = denomerator;
+//     while (a) {
+//         let c = b % a;
+//         b = a;
+//         a = c;
+//     }
+//     return [numerator / b, denomerator / b]
+// }
+
 
 //3
-function Third() {
-    let modulValue = document.getElementById('module-value').value;
-    if (modulValue < 0) {
-        modulValue = modulValue * -1;
-        document.getElementById('result-three').value = modulValue;
+
+let currentTime = new Date,
+    timeStart,
+    timeButton = document.querySelector('.add-time'),
+    myTimeObj = {
+        hours: 23,
+        minutes: 58,
+        seconds: 56,
+    };
+
+timeStart = setInterval(tick, 1000);
+
+timeButton.onclick = function () {
+    let addHour = +document.getElementById('hours').value,
+        addMinute = +document.getElementById('minutes').value,
+        addSecond = +document.getElementById('sec').value;
+    if (addTime(myTimeObj.seconds, addSecond) > 59) {
+        myTimeObj.seconds = (myTimeObj.seconds + addSecond) - 60;
+        let minuteCounter = myTimeObj.seconds;
+        for (let i = 0; minuteCounter > 0; minuteCounter -= 60) {
+            myTimeObj.minutes++;
+        }
+
     } else {
-        document.getElementById('result-three').value = modulValue;
+        myTimeObj.seconds += addSecond;
 
     }
-};
-
-//4
-
-function Fourth() {
-    let hourCheck = +document.getElementById('hour-value').value,
-        minCheck = +document.getElementById('min-value').value,
-        secCheck = +document.getElementById('sec-value').value;
-    if (hourCheck >= 0 && hourCheck <= 23 && minCheck >= 0 && minCheck <= 59 && secCheck >= 0 && secCheck <= 59) {
-        document.getElementById('result-four').value = `Время ok ${hourCheck}:${minCheck}:${secCheck}`;
-
-    } else {
-        document.getElementById('result-four').value = `Время введено не правильно!`;
-
-    }
-};
-
-//5
-
-function Fifth() {
-    let xValue = +document.getElementById('x-value').value,
-        yValue = +document.getElementById('y-value').value,
-        result;
-
-    if (xValue < 0 && yValue > 0) result = 'точка находится в I-й четверти';
-    if (xValue > 0 && yValue > 0) result = 'точка находится в II-й четверти';
-    if (xValue < 0 && yValue < 0) result = 'точка находится в III-й четверти';
-    if (xValue > 0 && yValue < 0) result = 'точка находится в IV-й четверти';
-    if (xValue === 0) result = 'точка находится на оси X';
-    if (yValue === 0) result = 'точка находится на оси Y';
-    if (xValue === 0 && yValue === 0) result = 'точка находится в начале координат';
-    document.getElementById('result-five').value = result;
-};
-
-//6 switch
-
-function Sixth() {
-    let month = +document.getElementById('month').value,
-        result;
-    switch (month) {
-        case 1:
-            result = 'January';
-            break;
-        case 2:
-            result = 'February';
-            break;
-        case 3:
-            result = 'March';
-            break;
-        case 4:
-            result = 'April';
-            break;
-        case 5:
-            result = 'May';
-            break;
-        case 6:
-            result = 'June';
-            break;
-        case 7:
-            result = 'July';
-            break;
-        case 8:
-            result = 'August';
-            break;
-        case 9:
-            result = 'September';
-            break;
-        case 10:
-            result = 'October';
-            break;
-        case 11:
-            result = 'November';
-            break;
-        case 12:
-            result = 'December';
-            break;
-        default:
-            result = 'Нет такого месяца';
-    }
-    document.getElementById('result-six').value = result;
-};
-
-//7
-function Seventh() {
-    let firstNumber = +document.getElementById('first-value').value,
-        secondNumber = +document.getElementById('second-value').value,
-        actValue = document.getElementById('act-value').value,
-        result;
-    switch (actValue) {
-        case "+":
-            result = firstNumber + secondNumber;
-            break;
-        case "-":
-            result = firstNumber - secondNumber;
-            break;
-        case "*":
-            result = firstNumber * secondNumber;
-            break;
-        case "/":
-            result = firstNumber / secondNumber;
-            break;
-        default:
-            result = 'попробуйте снова';
-    }
-    console.log(actValue + typeof (actValue));
-
-
-    document.getElementById('result-seven').value = result;
-};
-
-//8
-
-function Eighth() {
-    let firstNumber = +document.getElementById('first-number').value,
-        secondNumber = +document.getElementById('second-number').value,
-        result;
-    result = firstNumber > secondNumber ? result = `Первое число ${firstNumber} больше`
-        :
-        firstNumber < secondNumber ? result = `Второе число ${secondNumber} больше`
-            :
-            secondNumber === firstNumber ? result = `Числа одинаковые` : result = `Повторите попытку`;
-
-    document.getElementById('result-eighth').value = result;
 }
-;
 
-
-// 9
-function Ninth() {
-
-    let checkNumber = document.getElementById('check-number').value,
-        result;
-    result = checkNumber % 5 === 0 ? result = `Число ${checkNumber} кратно 5-ти` : checkNumber % 5 !== 0 ?
-      result = result = `Число ${checkNumber} НЕ кратно 5-ти`: result = `Что-то пошло не так`;
-    document.getElementById('result-ninth').value = result;
-
-};
-
-//10
-
-function Tenth() {
-    let namePlanet = document.getElementById('planet-name').value,
-        result;
-    result = namePlanet == "Земля" ||namePlanet == "земля" || namePlanet == "ЗЕМЛЯ" || namePlanet == "Earth" || namePlanet == "earth"|| namePlanet == "EARTH"? result = "Привет, землянин!": result = "Привет, инопланетянин!";
-
-    document.getElementById('result-tenth').value = result;
-
+function addNull(check) {
+    if (check < 10) {
+        return '0' + check;
+    }
+    return check;
 }
+
+function addTime(a, b) {
+    return a + b;
+}
+
+
+function tick() {
+    if (myTimeObj.seconds < 60) myTimeObj.seconds++;
+    if (myTimeObj.seconds >= 60) {
+        myTimeObj.seconds = 0;
+        myTimeObj.minutes++;
+        if (myTimeObj.minutes >= 60) {
+            myTimeObj.minutes = 0;
+            myTimeObj.hours++;
+            if(myTimeObj.hours >= 24){
+                myTimeObj.hours = 0;
+            }
+        }
+    }
+    document.getElementById('clock').innerText = `${addNull(myTimeObj.hours)} : ${addNull(myTimeObj.minutes)} : ${addNull(myTimeObj.seconds)}`;
+}
+
+
